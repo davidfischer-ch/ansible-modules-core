027521bcab2491fa358c5de144009f9563981500
David Fischer <david.fischer.ch@gmail.com>
2016-06-06 16:26:02 +0200

    Convert booleans to lower-case in description

--- cloud/amazon/iam_cert.py
+++ cloud/amazon/iam_cert.py
@@ -65,9 +65,9 @@ options:
       - The path to the private key of the certificate in PEM encoded format.
   dup_ok:
     description:
-      - By default the module will not upload a certificate that is already uploaded into AWS. If set to True, it will upload the certificate as long as the name is unique.
+      - By default the module will not upload a certificate that is already uploaded into AWS. If set to true, it will upload the certificate as long as the name is unique.
     required: false
-    default: False
+    default: false
     aliases: []
   aws_secret_key:
     description:
